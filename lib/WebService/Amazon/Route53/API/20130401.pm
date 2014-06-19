4f971908648bd3e25a2152c23d7c7e6e8587e7ea
Michal Wojciechowski <odyniec@odyniec.net>
2014-06-19 15:45:23 +0200

    Proxy 20130401 API methods to version 20110505
    (Temporarily, until a proper implementation of 20130401 is ready.)

--- lib/WebService/Amazon/Route53/API/20130401.pm
+++ lib/WebService/Amazon/Route53/API/20130401.pm
@@ -3,4 +3,52 @@ package WebService::Amazon::Route53::API::20130401;
 use warnings;
 use strict;
 
+use WebService::Amazon::Route53::API;
+use parent 'WebService::Amazon::Route53::API';
+
+use WebService::Amazon::Route53::API::20110505;
+
+sub new {
+    my ($class, %args) = @_;
+
+    my $self = $class->SUPER::new(%args);
+
+    $self->{api_version} = '2013-04-01';
+    $self->{api_url} = $self->{base_url} . $self->{api_version} . '/';
+
+    return $self;    
+}
+
+sub list_hosted_zones {
+    return WebService::Amazon::Route53::API::20110505::list_hosted_zones(@_);
+}
+
+sub get_hosted_zone {
+    return WebService::Amazon::Route53::API::20110505::get_hosted_zone(@_);
+}
+
+sub find_hosted_zone {
+    return WebService::Amazon::Route53::API::20110505::find_hosted_zone(@_);
+}
+
+sub create_hosted_zone {
+    return WebService::Amazon::Route53::API::20110505::create_hosted_zone(@_);
+}
+
+sub delete_hosted_zone {
+    return WebService::Amazon::Route53::API::20110505::delete_hosted_zone(@_);
+}
+
+sub list_resource_record_sets {
+    return WebService::Amazon::Route53::API::20110505::list_resource_record_sets(@_);
+}
+
+sub change_resource_record_sets {
+    return WebService::Amazon::Route53::API::20110505::change_resource_record_sets(@_);
+}
+
+sub error {
+    return WebService::Amazon::Route53::API::20110505::error(@_);
+}
+
 1;
