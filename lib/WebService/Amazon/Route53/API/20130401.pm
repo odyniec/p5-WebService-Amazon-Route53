b5a6b3ea78458cbd039ef9ea0719a4d3e86a9f18
Michal Wojciechowski <odyniec@odyniec.net>
2014-06-20 14:36:27 +0200

    Implement the 2013-04-01 version of list_hosted_zones

--- lib/WebService/Amazon/Route53/API/20130401.pm
+++ lib/WebService/Amazon/Route53/API/20130401.pm
@@ -21,8 +21,116 @@ sub new {
     return $self;    
 }
 
+=head2 list_hosted_zones
+
+Gets a list of hosted zones.
+
+Called in scalar context:
+
+    $zones = $r53->list_hosted_zones(max_items => 15);
+
+Called in list context:
+
+    ($zones, $next_marker) = $r53->list_hosted_zones(marker => '456ZONEID',
+                                                     max_items => 15);
+    
+Parameters:
+
+=over 4
+
+=item * marker
+
+Indicates where to begin the result set. This is the ID of the last hosted zone
+which will not be included in the results.
+
+=item * max_items
+
+The maximum number of hosted zones to retrieve.
+
+=back
+
+Returns: A reference to an array of hash references, containing zone data.
+Example:
+
+    $zones = [
+        {
+            'id' => '/hostedzone/123ZONEID',
+            'name' => 'example.com.',
+            'caller_reference' => 'ExampleZone',
+            'config' => {
+                'comment' => 'This is my first hosted zone'
+            },
+            'resource_record_set_count' => '10'
+        },
+        {
+            'id' => '/hostedzone/456ZONEID',
+            'name' => 'example2.com.',
+            'caller_reference' => 'ExampleZone2',
+            'config' => {
+                'comment' => 'This is my second hosted zone'
+            },
+            'resource_record_set_count' => '7'
+        }
+    ];
+    
+When called in list context, it also returns the next marker to pass to a
+subsequent call to C<list_hosted_zones> to get the next set of results. If this
+is the last set of results, next marker will be C<undef>.
+
+=cut
+
 sub list_hosted_zones {
-    return WebService::Amazon::Route53::API::20110505::list_hosted_zones(@_);
+    my ($self, %args) = @_;
+    
+    my $url = $self->{api_url} . 'hostedzone';
+    my $separator = '?';
+    
+    if (defined $args{'marker'}) {
+        $url .= $separator . 'marker=' . uri_escape($args{'marker'});
+        $separator = '&';
+    }
+    
+    if (defined $args{'max_items'}) {
+        $url .= $separator . 'maxitems=' . uri_escape($args{'max_items'});
+    }
+    
+    my $response = $self->_request('GET', $url);
+    
+    if (!$response->{success}) {
+        $self->_parse_error($response->{content});
+        return undef;
+    }
+    
+    # Parse the returned XML data
+    my $data = $self->{'xs'}->XMLin($response->{content},
+        ForceArray => [ 'HostedZone' ]);
+    my $zones = [];
+    my $next_marker;
+    
+    foreach my $zone_data (@{$data->{HostedZones}->{HostedZone}}) {
+        my $zone = {
+            'id' => $zone_data->{Id},
+            'name' => $zone_data->{Name},
+            'caller_reference' => $zone_data->{CallerReference},
+            'resource_record_set_count' => $zone_data->{ResourceRecordSetCount},
+        };
+        
+        if (exists $zone_data->{Config}) {
+            $zone->{config} = {};
+            
+            if (exists $zone_data->{Config}->{Comment}) {
+                $zone->{config}->{comment} = $zone_data->{Config}->{Comment};
+            }
+        }
+        
+        push(@$zones, $zone);
+    }
+    
+    if (exists $data->{NextMarker}) {
+        $next_marker = $data->{NextMarker};
+    }
+    
+    return wantarray ? ($zones, $next_marker) : $zones;
 }
 
 sub get_hosted_zone {
